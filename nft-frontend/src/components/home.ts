f00808b4d482a7dd1c89e0d24ddd0f5d2218f4f6
Warren Schwartz <warren.schwartz@gmail.com>
2022-03-18 22:43:44 +0200

    fix merge

---
+++ nft-frontend/src/components/home.ts
@@ -0,0 +1,8 @@
+import { IRouteViewModel, Params } from "aurelia";
+
+export class Home implements IRouteViewModel {
+    async load(params: Params) {
+
+    }
+} 
+	
--- nft-frontend/src/my-app.ts
+++ nft-frontend/src/my-app.ts
@@ -4,8 +4,8 @@ import { Chain } from "./services/chain";
 
 @route({
   routes: [
-    { path: "", component: import("./components/mint"), title: "Home"},
-    { path: "generate", component: import("./components/generate"), title: "Generate"}
+    { path: ["", "home"], component: import("./components/home"), title: "Home"},
+    { path: "mint", component: import("./components/mint"), title: "Mint"}
   ]
 })
 
